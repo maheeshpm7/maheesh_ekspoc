0ebd5c1a5ef32778856970d443a6615691371d85
maheeshpm7 <104315264+maheeshpm7@users.noreply.github.com>
2024-03-06 16:19:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source                  = "./modules/eks"
-  aws_public_subnet       = module.vpc.aws_public_subnet
+  aws_private_subnet_ids  = module.vpc.aws_private_subnet_ids
   vpc_id                  = module.vpc.vpc_id
   cluster_name            = var.cluster_name
   endpoint_public_access  = false
@@ -22,10 +22,12 @@ module "vpc" {
   instance_tenancy = var.instance_tenancy
   vpc_cidr         = var.vpc_cidr
   # access_ip               = "0.0.0.0/0"
+  availability_zone       = var.availability_zone
   access_ip               = var.access_ip
   public_sn_count         = var.public_sn_count
-  private_sn_count        = var.private_sn_count
+  private_sn_count        = var.private_sn_count 
   public_cidrs            = var.public_cidrs
+  private_cidrs           = var.private_cidrs 
   map_public_ip_on_launch = var.map_public_ip_on_launch
   rt_route_cidr_block     = var.rt_route_cidr_block
 
@@ -35,7 +37,7 @@ module "bastionhost" {
   source    = "./modules/bastionhost"
   vpc_id    = module.vpc.vpc_id
   key_name  = var.key_name
-  subnet_id = module.vpc.aws_public_subnet
+  subnet_id = module.vpc.aws_public_subnet_ids 
   ami_id    = var.ami_id
   instance_type_BH = var.instance_type_BH
 }
