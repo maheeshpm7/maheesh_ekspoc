eaa038db57d964de95b9a1b89a87137d58313e4a
maheeshpm7 <104315264+maheeshpm7@users.noreply.github.com>
2024-03-06 16:27:08 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,8 +1,11 @@
 ###### vpc/outputs.tf 
-output "aws_public_subnet" {
-  value = aws_subnet.public_cloudnative_subnet.*.id
+output "aws_public_subnet_ids" {
+  value = aws_subnet.public_cloud-native_subnet.*.id
 }
 
+output "aws_private_subnet_ids" {
+  value = aws_subnet.public_cloud-native_subnet.*.id
+}
 output "vpc_id" {
-  value = aws_vpc.cloudnative.id
+  value = aws_vpc.cloud-native.id
 }
